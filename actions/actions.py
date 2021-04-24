41b106c762bc58590e02f2b80c4b017f769991ca
JAYASHREEPATIL <jayapatil9830@gmail.com>
2021-04-24 13:55:46 +0530

    custom actions and BOT UI updated by JP

--- actions/actions.py
+++ actions/actions.py
@@ -41,3 +41,87 @@ class ActionTPO(Action):
         dispatcher.utter_message("wait... you are redirected")
         webbrowser.open(tpo_url)
         return []
+
+
+class PlacementRecordOfThirdFourth(Action):
+    def name(self) -> Text:
+        return "place_rec_third"
+
+    async def run(
+            self,
+            dispatcher,
+            tracker: Tracker,
+            domain: "DomainDict",
+    ) -> List[Dict[Text, Any]]:
+        link_url = "http://tpo.geca.ac.in/PlacementRecords2013-14.aspx"
+        dispatcher.utter_message("wait... you are redirected")
+        webbrowser.open(link_url)
+        return []
+
+
+# placement record of 2014 and 2015
+class PlacementRecordOfFourthFifth(Action):
+    def name(self) -> Text:
+        return "placement_record_fourthfifth"
+
+    async def run(
+            self,
+            dispatcher,
+            tracker: Tracker,
+            domain: "DomainDict",
+    ) -> List[Dict[Text, Any]]:
+        link_url = "http://tpo.geca.ac.in/PlacementRecords2014-15.aspx"
+        dispatcher.utter_message("wait... you are redirected")
+        webbrowser.open(link_url)
+        return []
+
+
+# placement record of 2015 and 2016
+class PlacementRecordOfFifteen(Action):
+    def name(self) -> Text:
+        return "place_recordOf_fifteensixteen"
+
+    async def run(
+            self,
+            dispatcher,
+            tracker: Tracker,
+            domain: "DomainDict",
+    ) -> List[Dict[Text, Any]]:
+        link_url = "http://tpo.geca.ac.in/PlacementRecords2015-16.aspx"
+        dispatcher.utter_message("wait... you are redirected")
+        webbrowser.open(link_url)
+        return []
+
+
+# placement record of 2016 and 2017
+class PlacementRecordOfSeventeen(Action):
+    def name(self) -> Text:
+        return "placement_recordOf_serVenteen"
+
+    async def run(
+            self,
+            dispatcher,
+            tracker: Tracker,
+            domain: "DomainDict",
+    ) -> List[Dict[Text, Any]]:
+        link_url = "http://tpo.geca.ac.in/PlacementRecords2016-17.aspx"
+        dispatcher.utter_message("wait... you are redirected")
+        webbrowser.open(link_url)
+        return []
+
+
+# placement record of 2017 and 2018
+class PlacementRecordOfLast(Action):
+    def name(self) -> Text:
+        return "placeme_record_lastofYear"
+
+    async def run(
+            self,
+            dispatcher,
+            tracker: Tracker,
+            domain: "DomainDict",
+    ) -> List[Dict[Text, Any]]:
+        link_url = "http://tpo.geca.ac.in/PlacementRecords2017-18.aspx"
+        dispatcher.utter_message("wait... you are redirected")
+        webbrowser.open(link_url)
+        return []
