d1c61a1891d0748e83e7e3d4fc2f6288e8a035fb
prachinimkar1@gmail.com <prachinimkar1@gmail.com>
2021-04-30 17:10:24 +0530

    Departments Details added by Prachi

--- actions/actions.py
+++ actions/actions.py
@@ -125,3 +125,163 @@ class PlacementRecordOfLast(Action):
         dispatcher.utter_message("wait... you are redirected")
         webbrowser.open(link_url)
         return []
+
+class ActionCivil(Action):
+    def name(self) -> Text:
+        return "action_Civil_Details"
+
+    async def run(
+            self,
+            dispatcher,
+            tracker: Tracker,
+            domain: "DomainDict",
+    ) -> List[Dict[Text, Any]]:
+        civil_url = "http://geca.ac.in/dep-civil-home.aspx"
+        dispatcher.utter_message("wait... you are redirected")
+        webbrowser.open(civil_url)
+        return []
+
+
+class ActionElectrical(Action):
+    def name(self) -> Text:
+        return "action_ele_Details"
+
+    async def run(
+            self,
+            dispatcher,
+            tracker: Tracker,
+            domain: "DomainDict",
+    ) -> List[Dict[Text, Any]]:
+        ele_url = "http://geca.ac.in/dep-elect-home.aspx"
+        dispatcher.utter_message("wait... you are redirected")
+        webbrowser.open(ele_url)
+        return []
+
+
+class ActionMech(Action):
+    def name(self) -> Text:
+        return "action_Mech_Details"
+
+    async def run(
+            self,
+            dispatcher,
+            tracker: Tracker,
+            domain: "DomainDict",
+    ) -> List[Dict[Text, Any]]:
+        mech_url = "http://geca.ac.in/dep-mech-home.aspx"
+        dispatcher.utter_message("wait... you are redirected")
+        webbrowser.open(mech_url)
+        return []
+
+
+class ActionCSE(Action):
+    def name(self) -> Text:
+        return "action_CSE_Details"
+
+    async def run(
+            self,
+            dispatcher,
+            tracker: Tracker,
+            domain: "DomainDict",
+    ) -> List[Dict[Text, Any]]:
+        cse_url = "http://geca.ac.in/dep-cse-home.aspx"
+        dispatcher.utter_message("wait... you are redirected")
+        webbrowser.open(cse_url)
+        return []
+
+
+class ActionET(Action):
+    def name(self) -> Text:
+        return "action_ET_Details"
+
+    async def run(
+            self,
+            dispatcher,
+            tracker: Tracker,
+            domain: "DomainDict",
+    ) -> List[Dict[Text, Any]]:
+        ET_url = "http://geca.ac.in/dep-entc-home.aspx"
+        dispatcher.utter_message("wait... you are redirected")
+        webbrowser.open(ET_url)
+        return []
+
+
+
+class ActionITechology(Action):
+    def name(self) -> Text:
+        return "action_IT_Details"
+
+    async def run(
+            self,
+            dispatcher,
+            tracker: Tracker,
+            domain: "DomainDict",
+    ) -> List[Dict[Text, Any]]:
+        I_url = "http://geca.ac.in/dep-it-home.aspx"
+        dispatcher.utter_message("wait... you are redirected")
+        webbrowser.open(I_url)
+        return []
+
+
+class ActionMCA(Action):
+    def name(self) -> Text:
+        return "action_MCA_Details"
+
+    async def run(
+            self,
+            dispatcher,
+            tracker: Tracker,
+            domain: "DomainDict",
+    ) -> List[Dict[Text, Any]]:
+        mca_url = "http://geca.ac.in/dep-mca-home.aspx"
+        dispatcher.utter_message("wait... you are redirected")
+        webbrowser.open(mca_url)
+        return []
+
+
+class ActionScience(Action):
+    def name(self) -> Text:
+        return "action_Science_Details"
+
+    async def run(
+            self,
+            dispatcher,
+            tracker: Tracker,
+            domain: "DomainDict",
+    ) -> List[Dict[Text, Any]]:
+        science_url = "http://geca.ac.in/dep-asci-home.aspx"
+        dispatcher.utter_message("wait... you are redirected")
+        webbrowser.open(science_url)
+        return []
+
+
+class ActionMath(Action):
+    def name(self) -> Text:
+        return "action_Math_Details"
+
+    async def run(
+            self,
+            dispatcher,
+            tracker: Tracker,
+            domain: "DomainDict",
+    ) -> List[Dict[Text, Any]]:
+        math_url = "http://geca.ac.in/dep-math-home.aspx"
+        dispatcher.utter_message("wait... you are redirected")
+        webbrowser.open(math_url)
+        return []
+
+
+class ActionMechanics(Action):
+    def name(self) -> Text:
+        return "action_Mechanics_Details"
+
+    async def run(
+            self,
+            dispatcher,
+            tracker: Tracker,
+            domain: "DomainDict",
+    ) -> List[Dict[Text, Any]]:
+        mechanics_url = "http://geca.ac.in/dep-amech-home.aspx"
+        dispatcher.utter_message("wait... you are redirected")
+        webbrowser.open(mechanics_url)
+        return []
